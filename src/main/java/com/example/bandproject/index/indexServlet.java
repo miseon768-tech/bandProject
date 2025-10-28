4a5d6c0f7b20d6b794a696d43bb13df971ca29cd
miseon768-tech <miseon768@gmail.com>
2025-10-28 12:51:01 +0900

    index

--- src/main/java/com/example/bandproject/index/indexServlet.java
+++ src/main/java/com/example/bandproject/index/indexServlet.java
@@ -9,12 +9,12 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 
-@WebServlet("/main")
+@WebServlet("/index")
 public class indexServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 
-        req.getRequestDispatcher("/main.jsp").forward(req,resp);
+        req.getRequestDispatcher("/index.jsp").forward(req,resp);
     }
 }
