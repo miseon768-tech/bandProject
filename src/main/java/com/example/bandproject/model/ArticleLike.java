a07d26e1e52b06d90be508bade0fd2d86ed91fdd
miseon768-tech <miseon768@gmail.com>
2025-10-28 11:14:30 +0900

    자바 틀 잡기

---
+++ src/main/java/com/example/bandproject/model/ArticleLike.java
@@ -0,0 +1,13 @@
+package com.example.bandproject.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class ArticleLike {
+    int idx;
+    String memberId;
+    int articleNo;
+
+}
---
+++ src/main/java/com/example/bandproject/model/WriteCounter.java
@@ -0,0 +1,11 @@
+package com.example.bandproject.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class WriteCounter {
+    String writerId;
+    String writeCnt;
+}
