7c69872b3d05078faa7a12d69a9541a33532743d
miseon768-tech <miseon768@gmail.com>
2025-10-29 14:51:18 +0900

    bandMember model

--- src/main/java/com/example/bandproject/model/BandMember.java
+++ src/main/java/com/example/bandproject/model/BandMember.java
@@ -1,6 +1,15 @@
 package com.example.bandproject.model;
 
 
+import java.time.LocalDateTime;
 
 public class BandMember {
+
+int no;
+int band_no;
+String member_id;
+String role;
+boolean approved;
+LocalDateTime joined_at;
+
 }
