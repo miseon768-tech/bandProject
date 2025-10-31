377df19b609e107e56966320e8dce6754f2425be
miseon768-tech <miseon768@gmail.com>
2025-10-31 17:49:31 +0900

    band/list 수정 BandMapper.xml 수정

--- src/main/java/com/example/bandproject/band/BandListServlet.java
+++ src/main/java/com/example/bandproject/band/BandListServlet.java
@@ -12,17 +12,16 @@ import org.apache.ibatis.session.SqlSession;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet("/band/list?no={no}")
+@WebServlet("/band/list")
 public class BandListServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        String no = req.getParameter("no");
 
 
         SqlSession sqlSession = MyBatisUtil.build().openSession(true);
         List<Band> bandList
-                = sqlSession.selectList("mappers.BandMapper.selectByArticleNo", Integer.parseInt(no));
+                = sqlSession.selectList("mappers.BandMapper.selectAll");
         req.setAttribute("bandlist", bandList);
 
         sqlSession.close();
